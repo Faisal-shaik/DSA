9dcdc32b0d60a1e61099e9e361383876c189d126
Faisal-shaik <devanakondamohammadusama@gmail.com>
2026-03-29 21:40:20 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int ans=0;
+        int min=prices[0];
+        for(int i=1;i<n;i++){
+            int profit=prices[i]-min;
+                ans=Math.max(profit,ans);
+                min=Math.min(min,prices[i]);
+        }
+        return ans;
+    }
+}
